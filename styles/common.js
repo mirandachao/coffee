635b7b6db81d0b1af0dd4b03e7ee79a35e1631d6
Miranda Chao <miranda_chao@brown.edu>
2017-01-11 00:42:17 -0500

    Added some js dropdowns, need to fix styling for it and fix styling for author and title a-z pages

---
+++ styles/common.js
@@ -0,0 +1,41 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropdownAuthor() {
+    document.getElementById("dropdown_author").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropdownTitle() {
+    document.getElementById("dropdown_title").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
